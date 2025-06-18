bd3c4bf08f8ca7d77a530c843e3b29abe562a0a4
Nadeem-pc <nadeempc777@gmail.com>
2025-06-18 22:49:15 +0530

    Added db connecting code

---
+++ backend/src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { env } from './env';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const dbURI = env.MONGODB_URI;
+        if(!dbURI) {
+            throw new Error("MONGODB_URI is not defined in .env");
+        }
+        await mongoose.connect(dbURI);
+        console.log("Database connected successfully");
+        
+    } catch (error) {
+        console.error("Database Connection Error",error.message);
+    }
+};
+
+export default connectDB;
